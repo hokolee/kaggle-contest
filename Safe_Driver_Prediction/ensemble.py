4916d4919887732a2abaf0171728a46713c51dfa
lihonghong <lihonghong@xiaomi.com>
2017-11-18 22:15:14 +0800

    assert id is equal in ensemble

--- Safe_Driver_Prediction/ensemble.py
+++ Safe_Driver_Prediction/ensemble.py
@@ -3,7 +3,7 @@ import sys
 def read_ans_data(ans_file):
   with open(ans_file) as ans:
     data = []
-    for line in sorted(ans): data.append(line.strip().split(","))
+    for line in ans: data.append(line.strip().split(","))
     ans.close()
     return data
 
@@ -18,10 +18,11 @@ num = len(test_set)
 length = len(test_set[0])
 
 print("id,target")
-for i in range(0, length):
+for i in range(1, length):
   ans = 0.0
   for j in range(0, num):
     assert len(test_set[j][i]) == len(test_set[0][i])
+    assert test_set[0][i][0] == len(test_set[j][i][0])
     pro = float(test_set[j][i][1])
     ans += (1.0 / num) * pro
   result = "%s,%.4f" % (test_set[0][i][0], float(ans))
